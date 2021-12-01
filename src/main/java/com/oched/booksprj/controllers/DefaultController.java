5472400a2ae034d538c78daf4d1544f86b2cfb5d
Oched <oched.denis@gmail.com>
2021-12-01 21:15:17 +0200

    MASTER-2: added DefaultController

--- src/main/java/com/oched/booksprj/controllers/DefaultController.java
+++ src/main/java/com/oched/booksprj/controllers/DefaultController.java
@@ -3,11 +3,29 @@ package com.oched.booksprj.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/bla")
 public class DefaultController {
-    @GetMapping("/")
+
+    @GetMapping
     public ResponseEntity<String> index() {
-        return ResponseEntity.ok("Books PRJ");
+        return ResponseEntity.ok("Hello World!");
+    }
+
+    @GetMapping("/param")
+    public ResponseEntity<String> getParams(
+            @RequestParam String param1,
+            @RequestParam String param2,
+            HttpServletRequest request
+    ) {
+        String str = "param1 = " + param1 + " param2 = " + param2 +
+                "\nparam from servlet = " + request.getParameter("param2");
+
+        return ResponseEntity.ok(str);
     }
 }
