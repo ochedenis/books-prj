101186f47ece964232650acaa34412cac9f0e0ea
Oched <oched.denis@gmail.com>
2021-12-10 21:45:02 +0200

    HOME: update security

--- src/main/resources/db/changelog/mysql-changes/v0004_add_users.sql
+++ src/main/resources/db/changelog/mysql-changes/v0004_add_users.sql
@@ -3,6 +3,7 @@ CREATE TABLE users (
     login varchar(255) NOT NULL unique,
     email varchar(255) NOT NULL unique,
     password varchar(255) NOT NULL,
+    auth varchar(50),
     registered_on datetime default NOW(),
     PRIMARY KEY (id)
 );
